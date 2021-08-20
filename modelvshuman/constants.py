da73037adb85e64cb599794d18eb2bb43aa50d81
Robert Geirhos <23079422+rgeirhos@users.noreply.github.com>
2021-08-20 14:37:03 +0200

    Updated constants.py with environment variable info

--- modelvshuman/constants.py
+++ modelvshuman/constants.py
@@ -8,7 +8,7 @@ from os.path import join as pjoin
 ##################################################################
 
 PROJ_DIR = str(os.environ.get("MODELVSHUMANDIR", "model-vs-human"))
-assert (PROJ_DIR != "None"), "Please set the datasets path 'MODELVSHUMANDIR' environment variable"
+assert (PROJ_DIR != "None"), "Please set the 'MODELVSHUMANDIR' environment variable as described in the README"
 CODE_DIR = pjoin(PROJ_DIR, "modelvshuman")
 DATASET_DIR = pjoin(PROJ_DIR, "datasets")
 FIGURE_DIR = pjoin(PROJ_DIR, "figures")
